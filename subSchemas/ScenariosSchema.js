a383c301c4e3f8de3c792cf4810a269d92223cbc
alanlyra <alanlyra7@gmail.com>
2025-05-30 02:16:10 -0300

    feat(): scenarios generation

--- subSchemas/ScenariosSchema.js
+++ subSchemas/ScenariosSchema.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ScenariosSchema = new Schema({
-    plausible: String,
-    pessimistic: String,
-    optimistic: String,
-    plausibleGenerated: String,
-    pessimisticGenerated: String,
-    optimisticGenerated: String
+    scenarioType: String,
+    scenarioText: String,
+    positiveImpacts: [Schema.Types.Mixed],
+    negativeImpacts: [Schema.Types.Mixed]
 });
 
 module.exports = ScenariosSchema;
